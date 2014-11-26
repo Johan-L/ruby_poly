c96a55f3ee645854ac016601f8391b7f3e2acf22
Johan Lekberg <Johan.Lekberg@atex.com>
2014-11-26 15:18:12 +0100

    created a content help to get content

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
-	#include ::ContentHelper
 
 	require 'ContentHelper'
+	require 'json'	
+
+
     def new
     	puts "NEW"	  	
     	a = Article.new()
@@ -14,53 +16,17 @@ class ArticlesController < ApplicationController
 	def show
 require 'rest_client'
 		puts "SHOW"	
-		require 'json'
 
-		
-
-	  	a = Article.new()
-	  	a.title = "Title"
-	  	a.text = "Text"
-
-	  	response2 = RestClient.post('http://localhost:8080/onecms/security/token?format=json',
-  			{'username' => 'sysadmin',
-  			 'password' => 'sysadmin'
-  			}.to_json,
-			:content_type => 'application/json')
-
-	  	puts response2.inspect
-	  	paresed_response = JSON(response2)
-	  	token = paresed_response["token"]
-	  	puts token
-
-	  	major = params[:id]
+		major = params[:id]
 	  	minor = params[:format]
-	  	puts ContentHelper.get_content("#{major}.#{minor}")
 
-	  	url = "http://localhost:8080/onecms/content/contentid/#{major}.#{minor}?format=json"
-	  	puts url
+     	response = ContentHelper.get_content("#{major}.#{minor}")
 
-	  	response2 = RestClient.get(url,
-			:x_auth_token => token)
+	  	paresed_response = JSON(response)
 
-	  	#puts JSON(response2)
-	  	paresed_response = JSON(response2)
-
-	  	puts "TEST"
-	  	puts paresed_response["contentData"]["title"]
-
-	  	#response = RestClient.get 'http://sunet.se'
-	  	#puts response.inspect
-
-		
-		@element = {:title => "Title", :text => "Text"}
 		@article = Article.new
 		@article.title = paresed_response["contentData"]["title"]
 		@article.text = paresed_response
-		#@article.content = paresed_response
-		puts a.inspect
-
-
 	end
 
 	def create
--- app/helpers/contentHelper.rb
+++ app/helpers/contentHelper.rb
@@ -1,20 +1,14 @@
 module ContentHelper
   def self.get_content(str)
-
-  	puts ContentHelper.get_content("#{str}")
+  	puts "getContent"
   	response = RestClient.post('http://localhost:8080/onecms/security/token?format=json',
   			{'username' => 'sysadmin',
   			 'password' => 'sysadmin'
   			}.to_json,
 			:content_type => 'application/json')
 
-  	puts response.inspect
   	paresed_response = JSON(response)
   	token = paresed_response["token"]
-  	puts token
-
-  	major = params[:id]
-  	minor = params[:format]
 
 
   	url = "http://localhost:8080/onecms/content/contentid/#{str}?format=json"
